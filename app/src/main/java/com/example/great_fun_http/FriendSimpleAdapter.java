6318bc4768618ad2805466181dee1ca486d06c10
lb <lubing1630@163.com>
2018-12-25 15:04:19 +0800

    feat : 添加增加好友逻辑

--- app/src/main/java/com/example/great_fun_http/FriendSimpleAdapter.java
+++ app/src/main/java/com/example/great_fun_http/FriendSimpleAdapter.java
@@ -1,8 +1,10 @@
 package com.example.great_fun_http;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SimpleAdapter;
 
@@ -11,21 +13,45 @@ import com.squareup.picasso.Picasso;
 import java.util.List;
 import java.util.Map;
 
-public class FriendSimpleAdapter extends SimpleAdapter {
-    public FriendSimpleAdapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to) {
-        super(context, data, resource, from, to);
-    }
-
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View view = super.getView(position, convertView, parent);
-
-        ImageView img = (ImageView) view.getTag();
-        if (img == null) {
-            img = (ImageView) view.findViewById(R.id.searchFriendImg);
-            view.setTag(img);
-        }
-        String url = ((Map) getItem(position)).get("userHeadImg").toString();
-        Picasso.get().load(url).resize(800, 600).into(img);
-        return view;
-    }
-}
+//public class FriendSimpleAdapter extends SimpleAdapter {
+//    public FriendSimpleAdapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to) {
+//        super(context, data, resource, from, to);
+//    }
+//
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        View view = super.getView(position, convertView, parent);
+//
+//        ImageView img = (ImageView) view.getTag();
+//        Button button = (Button) view.getTag();
+//        if (img == null) {
+//            img = (ImageView) view.findViewById(R.id.searchFriendImg);
+//            view.setTag(img);
+//        }
+//        if (button == null) {
+//            button = (Button) view.findViewById(R.id.addFriend);
+//            view.setTag(button);
+//        }
+//        String url = ((Map) getItem(position)).get("userHeadImg").toString();
+//        Picasso.get().load(url).resize(800, 600).into(img);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String friendId = ((Map) getItem(position)).get("userId").toString();
+//
+//            }
+//        });
+//        return view;
+//    }
+//
+//    public class PostFriend extends AsyncTask<String, Void, String> {
+//        @Override
+//        protected String doInBackground(String... strings) {
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String s) {
+//            super.onPostExecute(s);
+//        }
+//    }
+//}
--- app/src/main/java/com/example/great_fun_http/SearchFriendActivity.java
+++ app/src/main/java/com/example/great_fun_http/SearchFriendActivity.java
@@ -1,18 +1,22 @@
 package com.example.great_fun_http;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import com.example.great_fun_http.httpHelper.HttpApiHelper;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,6 +25,7 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SearchFriendActivity extends AppCompatActivity {
 
@@ -28,6 +33,37 @@ public class SearchFriendActivity extends AppCompatActivity {
     EditText mSearchKeywordET;
     ListView mSearchFriendList;
     int userId;
+
+
+    public class FriendSimpleAdapter extends SimpleAdapter {
+        public FriendSimpleAdapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to) {
+            super(context, data, resource, from, to);
+        }
+
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            View view = super.getView(position, convertView, parent);
+
+            ImageView img = (ImageView) view.getTag();
+//            Button button = (Button) view.getTag();
+            Button button = (Button) view.findViewById(R.id.addFriend);
+            if (img == null) {
+                img = (ImageView) view.findViewById(R.id.searchFriendImg);
+                view.setTag(img);
+            }
+            String url = ((Map) getItem(position)).get("userHeadImg").toString();
+            Picasso.get().load(url).resize(800, 600).into(img);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String friendId = ((Map) getItem(position)).get("userId").toString();
+                    String params = String.format("{ \"userId\": %s, \"friendId\": %s, \"operation\": 1 }", userId, friendId);
+                    new PostFriend().execute(params);
+                }
+            });
+            return view;
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -110,6 +146,40 @@ public class SearchFriendActivity extends AppCompatActivity {
                     mSearchFriendList.setAdapter(adapter);
 
                 }
+            } catch(JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public class PostFriend extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String params = strings[0];
+            return HttpApiHelper.getApiData(params, "http://116.62.156.102:7080/android/postFriend");
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            JSONObject jsonBody;
+            int retcode = 0;
+            try {
+                jsonBody = new JSONObject(s);
+                retcode = jsonBody.getInt("retcode");
+                if (retcode == 0) {
+                    Toast.makeText(SearchFriendActivity.this, "添加成功", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(SearchFriendActivity.this, "添加失败", Toast.LENGTH_LONG).show();
+                }
+                List<HashMap<String, Object>> data = new ArrayList<HashMap<String, Object>>();
+                SimpleAdapter adapter = new FriendSimpleAdapter(
+                        SearchFriendActivity.this,
+                        data,
+                        R.layout.list_item_search_friend,
+                        new String[] {},
+                        new int[] {}
+                );
+                mSearchFriendList.setAdapter(adapter);
             } catch(JSONException e) {
                 e.printStackTrace();
             }
